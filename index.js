ddd7715d84b7eea6391e58d55d57c0aa43347d51
mayfor4 <diegoor.ti22@utsjr.edu.mx>
2023-11-11 14:03:42 -0600

    subir keys

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express=require("express");
-var cors=require("cors");
 var path=require("path");
+var cors=require("cors");
+
 var session=require("cookie-session");
 var rutasEmpleados=require("./rutas/empleadosRutas");
 var rutasProductos=require("./rutas/productosRutas");
@@ -12,6 +13,14 @@ app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(session({
+    name:"session",
+    keys:["aksfRSDFG54t45yt"],
+    maxAge: 24 * 60 * 60 *1000
+
+}));
+
+
 app.use("/",express.static(path.join(__dirname,"/web")));
 app.use("/", rutasEmpleados);
 app.use("/",rutasProductos);
